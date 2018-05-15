f05c6952069730834459fb6b44b0cacc0fb4f31c
swiftsync <swiftsynctest@gmail.com>
2018-05-14 22:59:34 -0700

    IssueId::639135 entityType::KanbanDefect Reviewer::admin Status::Done Comments::test
    Signed-off-by: swiftsync <swiftsynctest@gmail.com>

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class HelloWorld
+{
+	public static void main(String[] args) 
+	{
+		 System.out.println(getLocalCurrentDate());
+	}
+	private static Date getLocalCurrentDate()
+	{
+		return new Date();
+		return null	1111
+	}
+}
